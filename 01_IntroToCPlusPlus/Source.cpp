a2761057dcfa9992f521bfa6fac960ff01d295b6
Olena Iliushyn <lenailyshun@gmail.com>
2026-08-31 16:20:40 +0300

    Add project files.

---
+++ 01_IntroToCPlusPlus/Source.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main()
+{
+	cout << "Hello world\n";
+
+
+}
+	//   \n - перехід на новий рядок
+	//   \t - пробіли(табуляції)
+	//   \\ - \деяка інформація\
+	//   \' - 'деяка інформація'
+	//   \" - "деяка інформація"
+	//cout << "Hello world\n";
+	//cout << "\tHello world \n";
+	//cout << "\t\tHello world \n";
+	//cout << "\t\t\tHello world \n";
+	//cout << "\\Hello world\\ \n";
+	//cout << "'Hello world' \n";
+	//cout << "\"Hello world\" \n";
+	//cout << 'f';//  only one symbol
+
+
+
+
+
+
+
+
+
+	return 0;
+}
+
+
+
+
+
+
